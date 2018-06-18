4e19069098e9f4d2b778ef212a75c78a90c9373a
Benoit BRISSET <benoit.brisset@gmail.com>
2018-06-18 12:16:27 +0200

    add directions

--- src/include/crazy_maze/maze.h
+++ src/include/crazy_maze/maze.h
@@ -2,6 +2,7 @@
 
 #include <array>
 #include <utility>
+#include <algorithm>
 
 #include <termcolor/termcolor.hpp>
 
@@ -10,6 +11,7 @@ using namespace std;
 namespace cm {
 
 static const char default_value = 35;
+static const char visited_value = 8;
 
 /**
  * @brief maze
@@ -35,16 +37,15 @@ public:
 
 private:
 
-    enum class door_type {start, end};
-
+    vector<int> _direction;
     array<array<char, M>, N> _matrix;
-    pair<uint32_t, uint32_t> _start;
-    pair<uint32_t, uint32_t> _end;
+    pair<uint32_t, uint32_t> _entrance;
+    pair<uint32_t, uint32_t> _exit;
     std::mt19937 _rng;
 
     void initialize_matrix();
-    pair<uint32_t, uint32_t> create_random_door(const door_type);
-    void recursive_division_method();
+    uint32_t random_number(uint32_t min, uint32_t max);
+    void carve_passage(int cx = 0, int cy = 0);
 
 };
 
@@ -54,6 +55,9 @@ private:
 template<size_t N, size_t M>
 maze<N, M>::maze()
 {
+    _directions = {1,2,3,4};
+    _dx = {-1, 0, 1, 0};
+    _dy = {0, 1, 0 -1};
     _rng.seed(std::random_device()());
 }
 
@@ -73,9 +77,11 @@ template<size_t N, size_t M>
 void maze<N, M>::generate_random_maze()
 {
     initialize_matrix();
-    _start = create_random_door(door_type::start);
-    _end = create_random_door(door_type::end);
-    recursive_division_method();
+
+    _entrance = {random_number(0, N), 0};
+    _exit = {random_number(0, N), M};
+
+    carve_passage(random_number(0, N), random_number(0, M));
 }
 
 /**
@@ -109,24 +115,48 @@ void maze<N, M>::initialize_matrix()
  * @brief
  */
 template<size_t N, size_t M>
-pair<uint32_t, uint32_t> maze<N, M>::create_random_door(const door_type type)
+uint32_t maze<N, M>::random_number(uint32_t min, uint32_t max)
 {
-    // distribution in range [1, N]
-    std::uniform_int_distribution<std::mt19937::result_type> dist(1,N);
-    uint32_t val = dist(_rng);
-    if (type == door_type::start) {
-        return {val, 0};
-    } else {
-        return {val, M-1};
-    }
+    std::uniform_int_distribution<std::mt19937::result_type> dist(min,max);
+    return dist(_rng);
 }
 
 /**
  * @brief
+ * The depth-first search algorithm of maze generation is frequently implemented using backtracking:
+
+    Make the initial cell the current cell and mark it as visited
+
+    While there are unvisited cells
+
+    If the current cell has any neighbours which have not been visited
+        Choose randomly one of the unvisited neighbours
+        Push the current cell to the stack
+        Remove the wall between the current cell and the chosen cell
+        Make the chosen cell the current cell and mark it as visited
+    Else if stack is not empty
+        Pop a cell from the stack
+        Make it the current cell
  */
 template<size_t N, size_t M>
-void maze<N, M>::recursive_division_method()
+void maze<N, M>::carve_passage(int cx, int cy)
 {
+    // Current case is visited
+    _matrix[cx][cy] = visited_value;
+
+    // Shuffle direction
+    std::shuffle(_directions.begin(), _directions.end(), _rng);
+
+    // For each direction
+    for (auto & dir: _directions) {
+
+        nx = cx + ;
+        ny = cy + ;
+
+    }
+
+
+
 
 }
 
